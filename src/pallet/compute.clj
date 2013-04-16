62192a797e444251454db10c879b3a1610146b3d
Anthony <anthony@moocar.me>
2013-04-15 20:58:46 -0700

    ported 0.7 smartos derivations

--- src/pallet/compute.clj
+++ src/pallet/compute.clj
@@ -125,6 +125,7 @@ Provider specific options may also be passed."
       (derive :suse-base :linux)
       (derive :bsd-base :linux)
       (derive :gentoo-base :linux)
+      (derive :solaris :linux)
 
       ;; distibutions
       (derive :centos :rh-base)
@@ -139,7 +140,8 @@ Provider specific options may also be passed."
       (derive :arch :arch-base)
       (derive :gentoo :gentoo-base)
       (derive :darwin :bsd-base)
-      (derive :os-x :bsd-base)))
+      (derive :os-x :bsd-base)
+      (derive :smartos :solaris)))
 
 (defmacro defmulti-os
   "Defines a defmulti used to abstract over the target operating system. The
@@ -173,6 +175,7 @@ Provider specific options may also be passed."
     (#{:suse} os-family) :zypper
     (#{:gentoo} os-family) :portage
     (#{:darwin :os-x} os-family) :brew
+    (#{:smartos :solaris} os-family) :pkgin
     :else (throw
            (ex-info
             (format "Unknown packager for %s %s" os-family os-version)
@@ -209,6 +212,7 @@ Provider specific options may also be passed."
       (#{:ubuntu :debian :jeos} os-family) :debian
       (#{:centos :rhel :amzn-linux :fedora} os-family) :rh
       (#{:arch} os-family) :arch
+      (#{:smartos} os-family) :solaris
       (#{:suse} os-family) :suse
       (#{:gentoo} os-family) :gentoo
       (#{:darwin :os-x} os-family) :os-x
